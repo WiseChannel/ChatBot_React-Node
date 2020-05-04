92a46da7d8ab98a240fec621f1322b6e2f933fe6
Ilya Wise <ilyaphotoshop17@gmail.com>
2020-05-04 23:21:25 +0300

    [add] create request in dialogFlowRoutes

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,2 +1,8 @@
 API_KEY = '810291d8ca8e4ca4a2c98896d2602eb4'
 DEV_ACCESS_TOKEN = 'f417e12cd43d42f488f11a84b644aa84'
+
+module.exports = {
+    googleProjectID: 'newagent-weqrvy',
+    dialogFlowSessionID: 'react-bot-session',
+    dialogFlowSessionLanguageCode: 'en-US'
+}
--- backend/routes/dialogFlowRoutes.js
+++ backend/routes/dialogFlowRoutes.js
@@ -1,9 +1,38 @@
+const dialogFlow = require('dialogflow')
+const config = require('../config/config')
+const sessionClient = new dialogFlow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(config.dialogFlowSessionID, config.googleProjectID)
+
 module.exports = app => {
     app.get('/', (req,res) => {
         res.send({'hello': 'there'})
     })
 
     app.set('/api/df_text_query', (req,res) => {
+
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: req.body.text,
+                    languageCode: config.dialogFlowSessionLanguageCode
+                }
+            }
+        }
+        sessionClient.detectIntent(request)
+            .then(response => {
+                const result = response[0].queryResult
+                if (result.intent) {
+                    console.log(`Internet: ${result.intent.displayName}`)
+                } else {
+                    console.log('No intent')
+                }
+            })
+            .catch(err => {
+                console.log('Err: ', err)
+            })
+
         res.send({'name': 'text query'})
     })
 
